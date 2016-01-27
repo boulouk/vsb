9660afd9bcf6d7edda82f1cb3711f36ef467c32b
boulouk <bouloukg@yahoo.com>
2016-01-27 16:44:32 +0100

    new maven module for the generated binding components

---
+++ binding-components/weather-bc/src/main/java/eu/chorevolution/vsb/weather/BCStarter.java
@@ -0,0 +1,40 @@
+package eu.chorevolution.vsb.weather;
+
+import eu.chorevolution.vsb.bc.manager.BcManager;
+import eu.chorevolution.vsb.gmdl.utils.BcConfiguration;
+
+/**
+ * @author Georgios Bouloukakis (boulouk@gmail.com)
+ * 
+ *         BCManager.java Created: 27 janv. 2016 Description:
+ */
+public class BCStarter {
+
+	public static void main(String[] args) {
+
+		// Should parse the local config file and instantiate the
+		BcConfiguration configuration = new BcConfiguration(/* path/to/local/config/file */);
+
+		// test purpose: should be extract when parsing the config file
+		configuration.setComponentRole("SERVER");
+		configuration.setServiceAddress("http://93.62.202.227");
+		configuration.setServiceName("BindingComponent");
+		configuration.setTargetNamespace("eu.chorevolution.vsb.weather.bc");
+		// END test purpose
+
+		BcManager manager = new BcManager(configuration);
+
+		// should be called remotely via Manager REST interface
+		
+		manager.setEndpointAddress("http://localhost:8888/BindingComponent"); 
+		manager.start();
+
+		/*
+		 * try { Thread.sleep(10000); } catch (InterruptedException e) { // TODO
+		 * Auto-generated catch block e.printStackTrace(); }
+		 * 
+		 * manager.stop();
+		 */
+	}
+
+}
---
+++ binding-components/weather-bc/src/main/java/eu/chorevolution/vsb/weather/bc/BindingComponent.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package eu.chorevolution.vsb.weather.bc;
+
+/**
+ * @author Georgios Bouloukakis (boulouk@gmail.com)
+ *
+ * G.java Created: 27 janv. 2016
+ * Description:
+ */
+public class BindingComponent {
+
+}
