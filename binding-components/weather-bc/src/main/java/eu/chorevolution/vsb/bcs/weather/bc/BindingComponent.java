25086f95d8c3b8d2c6d866fecce4f25ea97cacf3
boulouk <bouloukg@yahoo.com>
2016-01-27 16:54:50 +0100

    package corrections

---
+++ binding-components/weather-bc/src/main/java/eu/chorevolution/vsb/bcs/weather/bc/BindingComponent.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package eu.chorevolution.vsb.bcs.weather.bc;
+
+/**
+ * @author Georgios Bouloukakis (boulouk@gmail.com)
+ *
+ * BindingComponent.java Created: 27 janv. 2016
+ * Description:
+ */
+public class BindingComponent {
+
+}
