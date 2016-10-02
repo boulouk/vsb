81b36c7c2b4929a8313a9513edb4fd30453cacc6
sidutta <siddhartha_dutta@live.com>
2016-10-02 14:37:47 +0530

    started working on mqtt-rest demo

--- playgrounds/client-server/rest-playground/src/main/java/eu/chorevolution/vsb/playgrounds/clientserver/rest/RestServer.java
+++ playgrounds/client-server/rest-playground/src/main/java/eu/chorevolution/vsb/playgrounds/clientserver/rest/RestServer.java
@@ -7,7 +7,6 @@ import org.restlet.Server;
 import org.restlet.data.Protocol;
 import org.restlet.representation.Representation;
 import org.restlet.representation.StringRepresentation;
-import org.restlet.resource.Get;
 import org.restlet.resource.ResourceException;
 import org.restlet.resource.ServerResource;
 
@@ -16,52 +15,50 @@ import org.restlet.resource.ServerResource;
  *
  */
 public class RestServer {
-  
-  private Component component;
-  private Server server;  
-  private Boolean serverOnline = false;
 
-  public void startServer(final int port) {
-    if (!this.serverOnline) {
-      this.server = new Server(Protocol.HTTP, port);
-      this.component = new Component();
-      this.component.getServers().add(server);
-      
-      try {
-        this.component.start();
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-    }
-  }
+	private Component component;
+	private Server server;  
+	private Boolean serverOnline = false;
 
-  public void mget(String scope) {
-    this.component.getDefaultHost().attach("/"+scope, RestServerResource.class);
-    this.component.getDefaultHost().attach("/power/" + "{power}", RestServerResource.class);
-  }
-  
-  public static class RestServerResource extends ServerResource {
-    @Override
-    protected Representation post(Representation entity) throws ResourceException {
-      String receivedText = null;
-      try {
-        receivedText = entity.getText();
-      } catch (IOException e1) {
-        e1.printStackTrace();
-      }
-      System.out.println("Server received: " + receivedText);
-//      System.err.println("Server responded: ss");
-      return new StringRepresentation("ss");
-    }
-    
-    @Override
-    protected Representation get() throws ResourceException {
-      String power = (String) this.getRequestAttributes().get("power");
-      System.out.println(power);
-      return new StringRepresentation("bon!");
-    }
-    
-  }
+	public void startServer(final int port) {
+		if (!this.serverOnline) {
+			this.server = new Server(Protocol.HTTP, port);
+			this.component = new Component();
+			this.component.getServers().add(server);
 
-  
+			try {
+				this.component.start();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void mget(String scope) {
+		this.component.getDefaultHost().attach("/"+scope, RestServerResource.class);
+		this.component.getDefaultHost().attach("/power/" + "{power}", RestServerResource.class);
+	}
+
+	public static class RestServerResource extends ServerResource {
+		@Override
+		protected Representation post(Representation entity) throws ResourceException {
+			String receivedText = null;
+			try {
+				receivedText = entity.getText();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			System.out.println("Server received: " + receivedText);
+			//      System.err.println("Server responded: ss");
+			return new StringRepresentation("ss");
+		}
+
+		@Override
+		protected Representation get() throws ResourceException {
+			String power = (String) this.getRequestAttributes().get("power");
+			System.out.println(power);
+			return new StringRepresentation("bon!");
+		}
+	}
+
 }
